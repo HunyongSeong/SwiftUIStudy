c9790dbbd6aa377f35ea9091bf725d274e82467f
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-25 17:05:51 +0900

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by David Goggins on 2023/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
